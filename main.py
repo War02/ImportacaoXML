47e4235f20d1b3074a9bc7bf7c5f9dc020974e42
Mateus Barreto <matbsantos77@gmail.com>
2024-02-27 15:13:14 -0300

    Puxando todos os campos pedidos

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
-import xml.etree.cElementTree as ET
-from xml.dom import minidom
-from Tools.scripts.dutree import display
+import xml.etree.ElementTree as ET
+
+def find_text(root, namespace, path):
+    try:
+        return root.find(path, namespace).text
+    except AttributeError:
+        return None
 
 xml_path = 'C:\\Users\\User\\Downloads\\TesteXml\\35190104774509000142550050000603691000603697-nfe.xml'
 tree = ET.parse(xml_path)
@@ -9,18 +13,19 @@ root = tree.getroot()
 
 ns = {'nfe': 'http://www.portalfiscal.inf.br/nfe'}
 
-xNome = root.find('.//nfe:dest/nfe:xNome', ns)
-fone = root.find('.//nfe:dest/nfe:fone', ns)
-xLgr = root.find('.//nfe:dest/nfe:xLgr', ns)
-xBairro = root.find('.//nfe:dest/nfe:xBairro', ns)
-xMun = root.find('.//nfe:dest/nfe:xMun', ns)
-uf = root.find('.//nfe:dest/nfe:xUF', ns)
-cep = root.find('.//nfe:dest/nfe:CEP', ns)
-cnpj = root.find('.//nfe:dest/nfe:CNPJ', ns)
-cPais = root.find('.//nfe:dest/nfe:cPais', ns)
-nro = root.find('.//nfe:dest/nfe:nro', ns)
+data = {
+    'xNome': find_text(root, ns, './/nfe:dest/nfe:xNome'),
+    'fone': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:fone'),
+    'xLgr': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:xLgr'),
+    'xBairro': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:xBairro'),
+    'xMun': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:xMun'),
+    'uf': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:UF'),
+    'cep': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:CEP'),
+    'cnpj': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:CNPJ'),
+    'cPais': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:cPais'),
+    'nro': find_text(root, ns, './/nfe:dest/nfe:enderDest/nfe:nro')
+}
 
-if xNome is not None:
-    print(xNome.text)
-else:
-    print("Tag 'xNome' não encontrada dentro da tag 'dest'")
+df = pd.DataFrame(data, index=[0])
+
+print(df)
