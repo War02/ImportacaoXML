7a3739358f98f3b4f196b0ec640b99b2ff61a8eb
Mateus Barreto <matbsantos77@gmail.com>
2024-03-05 12:27:38 -0300

    Verificação de dados após a limpeza dos dados

---
+++ verifica_nomes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Verifica se todos os nomes que estão contidos em uma tabela, estão contidos na outra;
+
+def comparar_nome(planilha1_path, planilha2_path):
+    planilha1 = pd.read_excel(planilha1_path)
+    planilha2 = pd.read_excel(planilha2_path)
+
+    nomes_planilha1 = set(planilha1['CNPJ_CPF'])
+    nomes_planilha2 = set(planilha2['CNPJ_CPF'])
+
+    nome_somente_planilha1 = nomes_planilha1 - nomes_planilha2
+
+    nome_somente_planilha2 = nomes_planilha2 - nomes_planilha1
+
+    if not nome_somente_planilha1 and not nome_somente_planilha2:
+        print("\nTodos os nomes existem")
+
+    else:
+        print("\nNomes somente planilha 1:")
+        for nome in nome_somente_planilha1:
+            print(nome)
+
+        print("\nNomes somente planilha 2:")
+        for nome in nome_somente_planilha1:
+            print(nome)
+
+comparar_nome('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI(1).xlsx', 'C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI(1) - Copia.xlsx')
