bece497d99b98201ffd39596b08833908e590add
Mateus Barreto <matbsantos77@gmail.com>
2024-03-07 16:14:08 -0300

    Concatenando colunas

--- concatena_colunas.py
+++ concatena_colunas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# Concatena duas colunas gerando uma nova e separando os itens por vírgula
+
 planilha_emails = pd.read_excel('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\CORRESPONDENCIASFORNECEDORES.xlsx')
 
 planilha_emails['EMAIL'] = planilha_emails['email'].astype(str) + ',' + planilha_emails['email-nf'].astype(str)
--- identifica_emails.py
+++ identifica_emails.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Busca na planilha principal, pelo código da empresa e, o compara com outra planilha para dectar o e-mail do responsavel
-# salvando as informações na planilha de contatos
+# salvando as informações em uma nova planilha
 try:
     planilha_empresas = pd.read_excel('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI - TESTE.xlsx')
-    planilha_emails = pd.read_excel('\\\\192.168.10.51\\Dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Coletados para Migração\\EMAILCLIENTES.xlsx')
+    planilha_emails = pd.read_excel('\\\\192.168.10.51\\Dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Coletados para Migração\\EMAILFORNECEDORES.xlsx')
     #planilha_importacao = pd.read_csv('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\CONTATO-EMAIL_MODELOONDEPERMANECE O COD ANTERI.csv')
 
     nome_coluna_planilha_empresas = 'Empresa'
@@ -15,7 +15,7 @@ try:
     colunas_desejadas = ['Codigo', 'email', 'email-nf']
     planilha_final = planilha_mesclada[colunas_desejadas]
 
-    planilha_final.to_excel('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\CORRESPONDENCIAS.xlsx', index=False)
+    planilha_final.to_excel('C:\\Users\\User\\Documents\\Docs - Importacao - Cigam\\CORRESPONDENCIASFORNECEDORES.xlsx', index=False)
 
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
