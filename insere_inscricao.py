1bc1d068b81a6d179aef89db59e7e9e376903b82
Mateus Barreto <matbsantos77@gmail.com>
2024-03-19 12:23:11 -0300

    removendo comentários desnecessários

--- insere_inscricao.py
+++ insere_inscricao.py
@@ -1,37 +1,15 @@
-# import pandas as pd
-#
-# def insere_codigo(planilha_principal_path, planilha_geral_path):
-#     # Leitura dos arquivos do Excel, especificando o tipo de dado para CNPJ_CPF como string
-#     planilha_principal = pd.read_excel(planilha_principal_path, dtype={'CNPJ_CPF': str})
-#     planilha_geral = pd.read_excel(planilha_geral_path, dtype={'CNPJ': str})
-#
-#     codigos_clientes = []
-#     for cnpj in planilha_principal['CNPJ_CPF']:
-#         linha_correspondente = planilha_geral[planilha_geral['CNPJ'] == cnpj]
-#         if not linha_correspondente.empty:
-#             codigo_cliente = linha_correspondente.iloc[0]['IE']
-#             codigos_clientes.append(codigo_cliente)
-#         else:
-#             codigos_clientes.append(None)
-#
-#     planilha_principal['Inscricao'] = codigos_clientes
-#
-#     planilha_principal.to_excel(planilha_principal_path, index=False)
-#
-# insere_codigo('\\\\192.168.10.51\\Dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Usados para Migração\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI - PRINCIPAL - Copia.xlsx', '\\\\192.168.10.51\\dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Coletados para Migração\\IE.xlsx')
-
 import pandas as pd
 
 def insere_codigo(planilha_principal_path, planilha_geral_path):
     # Leitura dos arquivos do Excel, especificando o tipo de dado para CNPJ_CPF como string
     planilha_principal = pd.read_excel(planilha_principal_path, dtype={'CNPJ_CPF': str})
-    planilha_geral = pd.read_excel(planilha_geral_path, dtype={'CNPJ_CPF': str})
+    planilha_geral = pd.read_excel(planilha_geral_path, dtype={'CNPJ': str})
 
     codigos_clientes = []
     for cnpj in planilha_principal['CNPJ_CPF']:
-        linha_correspondente = planilha_geral[planilha_geral['CNPJ_CPF'] == cnpj]
+        linha_correspondente = planilha_geral[planilha_geral['CNPJ'] == cnpj]
         if not linha_correspondente.empty:
-            codigo_cliente = linha_correspondente.iloc[0]['Inscricao']
+            codigo_cliente = linha_correspondente.iloc[0]['IE']
             codigos_clientes.append(codigo_cliente)
         else:
             codigos_clientes.append(None)
@@ -41,5 +19,4 @@ def insere_codigo(planilha_principal_path, planilha_geral_path):
     planilha_principal.to_excel(planilha_principal_path, index=False)
 
 
-insere_codigo('\\\\192.168.10.51\\Dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Usados para Migração\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI - PRINCIPAL - Copia - Copia.xlsx', '\\\\192.168.10.51\\dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Usados para Migração\\Docs - Importacao - Cigam\\CLIENTES SEM IE.xlsx')
-
+insere_codigo('\\\\192.168.10.51\\Dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Usados para Migração\\Docs - Importacao - Cigam\\EMPRESAS_MODELOONDEPERMANECE O COD ANTERI - PRINCIPAL - Copia.xlsx', '\\\\192.168.10.51\\dados\\Departamental\\TI\\!CIGAM\\Migracao\\Dados Coletados para Migração\\IE.xlsx')
